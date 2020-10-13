b62cfaba43dae3d5d6cbf32d51f8633fc5563610
Shivam <Shivam.Chandarana@softwire.com>
2020-10-13 17:33:06 +0100

    Refactoring to use wordlist rather than string

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -10,75 +11,64 @@ namespace FizzBuzz
             Run();
         }
 
-        /* Using a string to store output instead of write command so only one write is needed and no extra boolean var */
         static void Run()
         {
             
             for (int i = 1; i < 300; i++)
             {
-                var output = "";
+                List<string> output = new List<string>();
                 if (i % 3 == 0)
                 {
-                    output += "Fizz";
+                    output.Add("Fizz");
 
                 }
 
                 if (i % 5 == 0)
                 {
-                    output += "Buzz";
+                    output.Add("Buzz");
                 }
 
                 if (i % 7 == 0)
                 {
-                    output += "Bang";
+                    output.Add("Bang");
                 }
 
                 if (i % 11 == 0)
                 {
-                    output = "Bong";
+                    output.Clear();
+                    output.Add("Bong");
                 }
 
                 if (i % 13 == 0)
                 {
-                    var firstB = output.IndexOf("B");
-                    if (firstB == -1)
+                    var firstB = output.FirstOrDefault(s => s.StartsWith("B"));
+                    if (firstB == null)
                     {
-                        output += "Fezz";
+                        output.Add("Fezz");
                     }
                     else
                     {
-                        output = output.Insert(firstB, "Fezz");
+                        output.Insert(output.IndexOf(firstB), "Fezz");
                     }
 
                 }
 
-                /* Splits output string into words of length 4 then reverses the order. Only works as all words Fizz, Fezz etc are of length 4.
-                 Would likely need to use a List instead of string for var output and change the way Fezz placement is calculated if wanting to allow for words of any length
-                 
-                 */
+
                 if (i % 17 == 0)
                 {
-                    var words = output.Length / 4;
-                    List<string> wordList = new List<string>();
-                    for (int j = 0; j < words; j++)
-                    {
-                        wordList.Add(output.Substring(j * 4, 4));
-                        
-                    }
-                    wordList.Reverse();
-                    output = "";
-                    foreach (var word in wordList)
-                    {
-                        output += word;
-                    }
+                    output.Reverse();
                 }
 
-                if (string.IsNullOrEmpty(output))
+                if (!output.Any())
                 {
-                    output += i;
+                    output.Add(i.ToString());
                 }
 
-                Console.WriteLine(output);
+                foreach (var word in output)
+                {
+                    Console.Write(word);
+                }
+                Console.WriteLine();
 
             }
         }
